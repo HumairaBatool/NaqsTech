102a96d3960161ab248ea0a248a8c96586f0b812
HumairaBatool <l201186@lhr.nu.edu.pk>
2023-08-16 23:51:28 +0500

    Bolg-Page-Responsive

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
-import image from '../img/bannerimg.webp';
-
-import image1 from '../img/wp.webp';
-import image2 from '../img/analysis.webp';
-import image3 from '../img/digitals.webp';
-
-import image4 from '../img/creative.webp';
-import image5 from '../img/e-commerce.webp';
-import image6 from '../img/digitalmarketing.webp';
+import image from "../img/bannerimg.webp";
+import BlogPosts from "./BlogPosts";
 
 class Blog extends Component {
   render() {
@@ -17,15 +10,17 @@ class Blog extends Component {
     };
     return (
       <div style={mystyle}>
-        <div className="h-12 bg-[#F48029]"></div>
-  
-        <div className="flex mx-auto bg-[#F48029] grid grid-cols-2">
+        <div className="xs:h-0  md:h-12 bg-[#F48029]"></div>
+        <div className="md:pb-28 xs:pb-20 flex-col relative  mx-auto bg-[#F48029] grid lg:grid-cols-2 xs:grid-cols-1">
           {/* First Column */}
-          <div className=" mt-6 h-[700px] lg:ml-20 text-white">
+          <div className=" mt-6 lg:ml-20 xs:ml-7 text-white">
             <h1 className="md:text-[22px] xs:text-[16px]">Our Latest News</h1>
-            <h1 className="font-bold mt-6  lg:text-5xl md:text-4xl xss:text-3xl xs:text-lg lg:!leading-[55px] xss:!leading-[40px] xs:!leading-[30px]">Blogs</h1>
+            <h1 className="font-bold mt-6  lg:text-5xl md:text-4xl xss:text-3xl xs:text-lg lg:!leading-[55px] xss:!leading-[40px] xs:!leading-[30px]">
+              Blogs
+            </h1>
             <p className="md:text-[25px] xs:text-[16px] leading-10 mt-6">
-              We deliver results-driven digital solutions to businesses of all sizes.
+              We deliver results-driven digital solutions to businesses of all
+              sizes.
             </p>
             <button className="mt-8 bg-[#363636] text-[18px] p-2 w-60 rounded-md h-12 align-middle hover:bg-white hover:text-black">
               Get Free Consultation
@@ -34,122 +29,16 @@ class Blog extends Component {
 
           {/* Second Column */}
           <div>
-            <img className="flex-grow  mt-8 w-[600px] h-[500px] pb-6" src={image} alt="Banner" />
+            <img
+              className="flex-col  mt-8 xs:px-10  flex justify-center xs:w-full  "
+              src={image}
+              alt="Banner"
+            />
           </div>
         </div>
 
         {/* Section 2 */}
-
-        <h1 className="text-center mt-14 mb-28 text-[52px] text-[#363636] font-extrabold">Our Blog posts</h1>
-
-        {/* Three-Column Layout */}
-        <div className="flex justify-center ml-20 mr-20 -space-x-6">
-          {/* First Column */}
-          <div className="w-1/3 px-4">
-                <div className="group relative">
-                <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                    <div class="max-w-[400px] overflow-hidden rounded-md">
-                        <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                            <img src={image1} class="hover:scale-[120%] transition duration-500 cursor-pointer object-cover"/>
-                            <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16 ">5 Key Elements of Effective WordPress Web Development Services in California</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                    In the dynamic digital landscape of California, effective web development services in California are crucial for businesses to establish a solid online …
-                </p>
-            </div>
-
-            {/* Second Column */}
-            <div className="w-1/3 px-4">
-                    <div className="group relative">
-                        <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                            <div class="max-w-[400px] overflow-hidden rounded-md">
-                                <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                                    <img src={image2} class="hover:scale-[120%] transition duration-500  cursor-pointer object-cover"/>
-                                    <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16 ">Why is Google Analytics Essential to SEO Services in California</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                    In the competitive online landscape, search engine optimization (SEO) plays a crucial role in driving organic traffic and improving the visibility of …
-                    </p>
-            </div>
-
-            {/* Third Column */}
-            <div className="w-1/3 px-4">
-                    <div className="group relative">
-                        <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                            <div class="max-w-[400px] overflow-hidden rounded-md">
-                                <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                                    <img src={image3} class="hover:scale-[120%] transition duration-500 cursor-pointer object-cover"/>
-                                    <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16 ">How a Digital Marketing Agency in California Can Help Your SEO Efforts</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                        In today’s digital world, organizations must have a strong online presence in order to live and succeed. Search Engine Optimization (SEO) is …
-                    </p>
-            </div>
-        </div>
-
-        {/*2nd Three-Column Layout */}
-
-        <div className="flex justify-center ml-20 mr-20 pb-20 -space-x-6 mt-12">
-          {/* First Column */}
-          <div className="w-1/3 px-4">
-                <div className="group relative">
-                <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                    <div class="max-w-[400px] overflow-hidden rounded-md">
-                        <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                            <img src={image4} class="hover:scale-[120%] transition duration-500 cursor-pointer object-cover"/>
-                            <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16">How to Choose the Perfect Graphic Design Services in California for Your Brand?</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                    Graphic design plays a crucial role in shaping your brand’s visual identity. Whether creating a captivating logo, designing a stunning website, or …
-                </p>
-            </div>
-
-            {/* Second Column */}
-            <div className="w-1/3 px-4">
-                    <div className="group relative">
-                        <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                            <div class="max-w-[400px] overflow-hidden rounded-md">
-                                <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                                    <img src={image5} class="hover:scale-[120%] transition duration-500 cursor-pointer object-cover"/>
-                                    <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16 ">7 Ways Ecommerce Marketing Services in California Can Boost Your Online Sales</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                        Online sales are getting increasingly prevalent in today’s digital age. The rivalry is becoming more difficult as Ecommerce marketing services in California …
-                    </p>
-            </div>
-
-            {/* Third Column */}
-            <div className="w-1/3 px-4">
-                    <div className="group relative">
-                        <div class="flex items-center space-y-2 flex-col justify-center mr-2">
-                            <div class="max-w-[400px] overflow-hidden rounded-md">
-                                <div class="max-w-[800px] max-h-[400px] overflow-hidden">
-                                    <img src={image6} class="hover:scale-[120%] transition duration-500 cursor-pointer object-cover"/>
-                                    <h2 className="text-[22px] font-[700] mt-8 ml-8 mr-16">How Can a Digital Marketing Agency in California Transform Your Business</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <p className="mt-5 ml-8 mr-16 text-[20px] text-[#7A7A7A]">
-                        In today’s extremely competitive digital market, businesses that want to succeed and stay ahead of the curve must have a strong online …
-                    </p>
-            </div>
-        </div>
+        <BlogPosts></BlogPosts>
       </div>
     );
   }
---
+++ src/Components/BlogPosts.js
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from "prop-types"; 
+import image1 from "../img/wp.webp";
+import image2 from "../img/analysis.webp";
+import image3 from "../img/digitals.webp";
+
+import image4 from '../img/creative.webp';
+import image5 from '../img/e-commerce.webp';
+import image6 from '../img/digitalmarketing.webp';
+
+function BlogItem({ imageSrc, title, content }) {
+  return (
+    <div className="shadow-lg overflow-hidden rounded bg-white">
+    <div className="relative">
+      <img src={imageSrc} alt="" class="hover:scale-110 w-full h-full transition duration-500 cursor-pointer object-cover"/>
+      </div>
+      <h3 className="md:p-8 xs:p-7 md:text-[23px] xs:text[16px] font-[700]">{title}</h3>
+      <p className="md:px-8 xs:px-7 md:pb-0 xs:pb-7 md:text-[20px] xs:text-[14px] ">{content}</p>
+    </div>
+  );
+}
+
+BlogItem.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+function BlogPosts() {
+  const blogItems = [
+    {
+      imageSrc: image1,
+      title:
+        "5 Key Elements of Effective WordPress Web Development Services in California",
+      content:
+        "In the dynamic digital landscape of California, effective web development services in California are crucial for businesses to establish a solid online …",
+    },
+    {
+      imageSrc: image2,
+      title: "Why is Google Analytics Essential to SEO Services in California",
+      content:
+        "In the competitive online landscape, search engine optimization (SEO) plays a crucial role in driving organic traffic and improving the visibility of …",
+    },
+    {
+      imageSrc: image3,
+      title: "How a Digital Marketing Agency in California Can Help Your SEO Efforts",
+      content:
+        "In today's digital world, organizations must have a strong online presence in order to live and succeed. Search Engine Optimization (SEO) is …",
+    },
+    {
+        imageSrc: image4,
+        title: "How to Choose the Perfect Graphic Design Services in California for Your Brand?",
+        content:
+        'Graphic design plays a crucial role in shaping your brand’s visual identity. Whether creating a captivating logo, designing a stunning website, or …'      },
+      {
+        imageSrc: image5,
+        title: "7 Ways Ecommerce Marketing Services in California Can Boost Your Online Sales",
+        content:
+           'Online sales are getting increasingly prevalent in today’s digital age. The rivalry is becoming more difficult as Ecommerce marketing services in California …'      },
+      {
+        imageSrc: image6,
+        title: "How Can a Digital Marketing Agency in California Transform Your Business",
+        content:
+            'In today’s extremely competitive digital market, businesses that want to succeed and stay ahead of the curve must have a strong online …'      },
+
+
+  ];
+
+  return (
+    <div>
+      <div className="max-auto">
+        <div className="relative">
+        <div className="md:pt-20 xs:pt-8 md:pb-0 xs:pb-8 text-center md:text-[45px] xs:text-[26px] md:font-[700] xs:font-[700]">
+        Our Blog posts
+      </div>
+
+          <div className=" xs:p-7 md:p-20 gap-6 grid lg:grid-cols-3 xs:grid-cols-1 auto-rows-fr">
+            {blogItems.map((item, index) => (
+              <BlogItem
+                key={index}
+                imageSrc={item.imageSrc}
+                title={item.title}
+                content={item.content}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlogPosts;
