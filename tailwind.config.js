a1c4e0a2cd4b76731213580dd9e9aef3d7f9889c
mianmir12 <jeehamza09@gmail.com>
2023-08-18 23:01:24 +0500

    pablo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,16 @@ module.exports = {
         'xs1': '354px',
         'xs2': '376px',// for Home page GET IN TOUCH
         'xs3': '426px',
-        'xs4': '540px',
         'xss': '480px',
+        'xs4': '540px',
         'xs6': '700px',
         'xs7': '885px',
-        'xs8': '1002px',
         'xs5': '600px',
+        'md':  '768px',
+        'xs8': '1002px',
         'lg': '1025px', // Adjust this breakpoint as needed
-        'md': '768px',
+        'xl': '1440px',
+        
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
