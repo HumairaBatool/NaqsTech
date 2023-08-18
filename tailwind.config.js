5c55ba492fb1c909319ac958834e9675dde340ff
moneebbutt1 <139575344+moneebbutt1@users.noreply.github.com>
2023-08-18 23:57:17 +0500

    tailwind-breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,14 @@ module.exports = {
         'xs1': '354px',
         'xs2': '376px',// for Home page GET IN TOUCH
         'xs3': '426px',
-        'xs4': '540px',
         'xss': '480px',
+        'xs4': '540px',
+        'xs5': '600px',
         'xs6': '700px',
+        'md': '768px',
         'xs7': '885px',
         'xs8': '1002px',
-        'xs5': '600px',
         'lg': '1025px', // Adjust this breakpoint as needed
-        'md': '768px',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
