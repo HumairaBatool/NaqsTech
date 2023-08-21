b6a754ce1e2bfb26e2a718073b4126004d728deb
HumairaBatool <l201186@lhr.nu.edu.pk>
2023-08-21 21:18:24 +0500

    About-Us

--- src/Components/About-Us.js
+++ src/Components/About-Us.js
@@ -12,39 +12,42 @@ import upimg from '../img/upwork.png';
 function AboutUs() {
   return (
     <div className=" max-auto">
-      <div className="bg-[#F48029] pb-5">
-        <div className=" max-auto grid lg:grid-cols-2 text-white sm:px-6 xl:px-20">
-          <div className=" text-left pr-12 overflow-hidden">
-            <p className="sm:pt-12 xl:pt-20 text-4xl font-bold  xl:leading-6 leading-10">
-              We Help Business to Grow{" "}
+
+          {/*1st Section */}
+      <div className="bg-[#F48029] pb-5 lg:pb-0">
+        <div className="relative xs:grid-cols-1 grid lg:grid-cols-2 text-white lg:pt-[30px]" >
+          <div className="xs:text-center md:text-left relative text-left overflow-hidden xs:px-[30px] xs:pb-[6px] xl:px-20 xs:pt-[40px] sm:pt-[0px] ">
+            <p className="xs:text-[31px] xs:leading-[28px] xs:font-[700] xs3:text-[32px] xs3:leading-[41.6px] md:text-[40px] md:leading-[40px] sm:pt-12">
+              We Help Business to Grow
             </p>
             <br />
-            <p className="text-xl pt-2 leading-loose">
+            <p className=" xs:text-[14px] xs:leading-[24px] md:text-[20px] md:leading-[40px] pt-2">
               NaqsTech is a full-service creative agency. We handle your design,
               engineering, and marketing from idea to launch. We are here to
               give you top-rated, up-to-date services with the latest trends,
               updated knowledge, and modern technologies.
             </p>
-            <div className="py-12">
-              <button className=" xl:py-3 px-5 rounded bg-black p-1 hover:bg-white hover:text-black text-white">
+            <div className="xs:py-12  md:py-8">
+              <button className="relative  xs:py-3 px-5 rounded bg-black p-1 hover:bg-white hover:text-black text-white">
                 Get Free Consultation
               </button>
             </div>
           </div>
-          <div>
-            <img className="xl:pt-20 sm:pt-12 xs:nw-full" src={img2} alt="" />
+          <div className="align-middle md:px-[24px] relative lg:p-[20px] flex items-center justify-center">
+            <img className="xs:pb-10  sm:pb-0 xs:w-full " src={img2} alt="" />
           </div>
         </div>
       </div>
 
-      <div className=" max-auto grid grid-cols-3 px-20">
+       {/*1st Section */}
+      <div className=" max-auto grid md:grid-cols-3 xs:grid-cols-1  xs:px-[30px] xs:pt-[20px] md:pt-[40px] lg:pt-[50px] relative ">
         <div>
-          <div className="flex pt-20">
-            <div className="text-4xl text-black font-bold pr-2">Our</div>
-            <div className="text-4xl text-black font-semibold"> Vision</div>
+          <div className="flex xs:pt-2">
+            <div className="text-[32px] leading-[38.4px] font-[500] text-black  pr-2">Our</div>
+            <div className="text-[32px] leading-[38.4px] font-[500] text-black"> Vision</div>
           </div>
-          <img className="py-10" src={img1} alt="" />
-          <div className="text-4xl text-black font-semibold">
+          <img className="xs:py-8 w-full" src={img1} alt="" />
+          <div className="xs:text-[24px] md:text-[25px] lg:text-[30px] text-black xs:leading-[43px] md:leading-[45px]">
             Zain-ul Abideen | CEO
           </div>
           <div>
@@ -74,8 +77,8 @@ function AboutUs() {
             </div>
           </div>
         </div>
-        <div className="col-span-2 text-left  text-black pl-20">
-          <p className="pt-20 text-2xl leading-10">
+        <div className="col-span-2 text-left  text-black md:px-[30px]">
+          <p className="xs:pt-20 md:pt-0 xs:text-[14px] xs:leading-[25px] md:text-[18px] md:leading-[35px] lg:text-[20px] lg:leading-[40px] ">
             Our vision is to empower businesses and organizations with the
             digital solutions they need to thrive in the ever-evolving online
             landscape. We envision a world where businesses of all sizes can
@@ -87,7 +90,7 @@ function AboutUs() {
             success.
           </p>
           <br />
-          <p className="text-3xl font-semibold pt-2 leading-10">
+          <p className="xs:text-[17px] xs:leading-[32px] xs3:text-[20px] xs3:leading-[43px] md:text-[25px] md:leading-[45px] lg:text-[30px] xs:font-[500] leading-10">
             “Our goal is to build a solution that give customer-facing teams at
             SMBs the ability to create fruitful and enduring relationships with
             customers.”
@@ -95,17 +98,20 @@ function AboutUs() {
         </div>
       </div>
 
-      <div className="pt-20 px-20">
-        <div className="flex items-center pt-5">
-          <div className=" text-3xl font-extrabold pl-5 pr-2 ">
+
+       {/*1st Section */}
+      <div className="xs:pt-5 xs:px-[30px]">
+        <div className="xs3:flex items-center">
+          <div className="xs:text-[28px] xs:leading-[43px] xs:font-[700] md:text-[35px] md:leading-[70px] ">
             GET IN TOUCH
           </div>
-          <div className="w-20 h-0.5 bg-black my-5 "></div>
+          <div className="pl-5">
+          <div className="w-14 h-0.5 bg-black md:my-5 "></div></div>
         </div>
         <div>
           <form className="py-1">
-            <div className="grid grid-cols-2 pl-5">
-              <div class="flex flex-col pr-4 py-5">
+            <div className="grid xs3:grid-cols-2 xs:grid-cols-1 lg:grid-cols-1 xs3:gap-x-4">
+              <div class="flex flex-col lg:pr-4 xs3:pt-2">
                 <label for="name" class="hidden">
                   Your Name
                 </label>
@@ -114,10 +120,10 @@ function AboutUs() {
                   name="name"
                   id="name"
                   placeholder="Full Name"
-                  class="w-100 mt-2  px-3 py-3 bg-gray-100 text-black rounded"
+                  class="w-100 mt-2  px-3 xs:py-[13px] md:py-[17px] bg-[#FAFAFA] text-black rounded"
                 />
               </div>
-              <div class="flex flex-col py-5">
+              <div class="flex flex-col  xs:pt-2 lg:pt-0">
                 <label for="name" class="hidden">
                   Email
                 </label>
@@ -126,12 +132,12 @@ function AboutUs() {
                   name="name"
                   id="name"
                   placeholder="Email"
-                  class="w-100 mt-2 py-3 px-3  bg-gray-100 text-black  rounded"
+                  class="w-100 mt-2  xs:py-[13px] md:py-[17px] px-3  bg-[#FAFAFA] text-black  rounded"
                 />
               </div>
             </div>
-            <div className="grid grid-cols-2 pl-5 py-5">
-              <div class="flex flex-col pr-4">
+            <div className="grid xs3:grid-cols-2 xs:grid-cols-1 lg:grid-cols-1 xs3:gap-x-4">
+              <div class="flex flex-col lg:pr-4 xs:pt-2 ">
                 <label for="name" class="hidden">
                   Phone Number
                 </label>
@@ -140,16 +146,16 @@ function AboutUs() {
                   name="name"
                   id="name"
                   placeholder="Phone Number"
-                  class="w-100 mt-2 py-3 px-3  bg-gray-100 text-black  rounded"
+                  class="w-100 mt-2  xs:py-[13px] md:py-[17px] px-3  bg-[#FAFAFA] text-black  rounded"
                 />
               </div>
-              <div class="flex flex-col">
+              <div class="flex flex-col xs:pt-2">
                 <label for="name" class="hidden">
                   Select Service
                 </label>
                 <select
                   id="category"
-                  className="w-100 mt-2 py-3 px-3 bg-gray-100 text-black rounded"
+                  className="w-100 mt-2  xs:py-[13px] md:py-[17px] px-3 bg-[#FAFAFA] text-black rounded"
                 >
                   <option value="General Inquiry">Select Service</option>
                   <option value="Feedback">SEO</option>
@@ -161,7 +167,7 @@ function AboutUs() {
               </div>{" "}
             </div>
 
-            <div class="flex flex-col py-5 pl-5">
+            <div class="grid grid-cols-1 xs:py-4">
               <label for="name" class="hidden">
                 Your Comment
               </label>
@@ -170,10 +176,10 @@ function AboutUs() {
                 name="name"
                 id="name"
                 placeholder="Your Comment"
-                class="w-100 mt-2 pt-5 px-3 pb-40 h bg-gray-100 text-black  rounded"
+                class="w-100 mt-2 pt-5 px-3 pb-[200px] h bg-[#FAFAFA] text-black  rounded"
               />
             </div>
-            <div className="pt-5 pl-5">
+            <div className="pt-5">
               <button
                 type="submit"
                 className="py-4 px-8 rounded bg-[#F48029] p-1 text-white"
@@ -184,6 +190,8 @@ function AboutUs() {
           </form>
         </div>
       </div>
+
+      {/*1st Section */}
       <div>
         <LatestNews></LatestNews>
       </div>
