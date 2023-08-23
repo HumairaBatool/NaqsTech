8b3505ae2506c856e8409004dfdd311684f8d345
HumairaBatool <l201186@lhr.nu.edu.pk>
2023-08-24 00:14:15 +0500

    Graphic-Design-QA1

--- src/Components/E-Commerce-Responsive-Cards.js
+++ src/Components/E-Commerce-Responsive-Cards.js
@@ -3,21 +3,7 @@ import React, { Component } from "react";
 import img2 from "../img/icon5.webp";
 import img3 from "../img/icon2.webp";
 import img4 from "../img/icon3.webp";
-
-class InnerCard extends Component {
-  render() {
-    return (
-      <div className="xs:mx-[10px] xs:mb-[10px] rounded-md bg-white shadow-lg  text-center flex content-center items-center justify-center shadow-orange-300 ">
-        <div className="xs:px-[15px] xs:py-[22px] md:py-[50px]">
-          <span className="flex items-center justify-center content-center xs:mb-[15px] ">
-            <img src={this.props.image} alt="icon" />
-          </span>
-          <h3 className="xs:mt-[8px] xs:text-[14px] md:text-[18px] text-[#363636] ">{this.props.heading}</h3>
-        </div>
-      </div>
-    );
-  }
-}
+import InnerCard from "./InnerCard";
 
 function ECommerceResponsiveCards() {
   const datials = [
--- src/Components/Hidden-Text-GD.js
+++ src/Components/Hidden-Text-GD.js
@@ -10,36 +10,40 @@ const HideShowText = () => {
       return;
     }
 
-    // Close the previously opened column (if any)
-    if (openColumnIndex !== null) {
-      setColumnVisibility((prevVisibility) => {
-        const updatedVisibility = [...prevVisibility];
-        updatedVisibility[openColumnIndex] = false;
-        return updatedVisibility;
-      });
-    }
 
-    // Open the clicked column and update the openColumnIndex
+   // Close the previously opened column (if any)
+   if (openColumnIndex !== null) {
     setColumnVisibility((prevVisibility) => {
       const updatedVisibility = [...prevVisibility];
-      updatedVisibility[index] = true;
+      updatedVisibility[openColumnIndex] = false;
       return updatedVisibility;
     });
-    setOpenColumnIndex(index);
-  };
+  }
+
+  // Open the clicked column and update the openColumnIndex
+  setColumnVisibility((prevVisibility) => {
+    const updatedVisibility = [...prevVisibility];
+    updatedVisibility[index] = true;
+    return updatedVisibility;
+  });
+  setOpenColumnIndex(index);
+};
 
   return (
-    <div className="grid md:grid-cols-4 xs:grid-cols-1 gap-5 lg:mx-20 xs:mx-[20px] text-center mb-20 ">
+    <div className="grid md:grid-cols-4 xs:grid-cols-1 md:gap-5 xs:gap-0 text-left ">
+
+
+
       <div>
         <button
           onClick={() => handleTextToggle(0)}
-          className={`text-black cursor-pointer ${openColumnIndex === 0 ? 'font-black' : ''}`}
+          className={`cursor-pointer ${openColumnIndex === 0 ? 'font-normal text-black' : 'text-[#B0AFAF]'}`}
         >
-          <div className="text-gray-500 text-2xl">Research</div>
+          <div className="text-[#B0AFAF] text-[20px]">Research</div>
         </button>
         {columnVisibility[0] && (
-          <div className="mt-10">
-            <p className="text-2xl">
+          <div className="xs:py-[20px]">
+            <p className="xs:text-[14px] md:text-[20px]">
               We gather information about the project, including the client’s
               goals, and design requirements. We research to understand the
               industry, competitors, and trends, gaining insights that inform
@@ -52,13 +56,13 @@ const HideShowText = () => {
       <div>
         <button
           onClick={() => handleTextToggle(1)}
-          className={`text-black cursor-pointer ${openColumnIndex === 1 ? 'font-black' : ''}`}
+          className={`cursor-pointer ${openColumnIndex === 0 ? 'font-normal text-black' : 'text-[#B0AFAF]'}`}
         >
-          <div className="text-gray-500 text-2xl">Concept Development</div>
+          <div className="text-[#B0AFAF] text-[20px]">Concept Development</div>
         </button>
         {columnVisibility[1] && (
-          <div className="mt-10">
-            <p className="text-2xl ">
+          <div className="xs:py-[20px]">
+            <p className="xs:text-[14px] md:text-[20px]">
               After gathering information, we explore various directions to
               create initial design concepts that align with the project’s goals
               and reflect the brand’s identity.
@@ -70,13 +74,13 @@ const HideShowText = () => {
       <div>
         <button
           onClick={() => handleTextToggle(2)}
-          className={`text-black cursor-pointer ${openColumnIndex === 2 ? 'font-black' : ''}`}
+          className={`cursor-pointer ${openColumnIndex === 0 ? 'font-normal text-black' : 'text-[#B0AFAF]'}`}
         >
-          <div className="text-gray-500 text-2xl">Design Creation</div>
+          <div className="text-[#B0AFAF] text-[20px]">Design Creation</div>
         </button>
         {columnVisibility[2] && (
-          <div className="mt-10">
-            <p className="text-2xl ">
+          <div className="xs:py-[20px]">
+            <p className="xs:text-[14px] md:text-[20px] ">
               After research and concept development, we create the required
               design and send it to our client for feedback or approval.
             </p>
@@ -87,19 +91,20 @@ const HideShowText = () => {
       <div>
         <button
           onClick={() => handleTextToggle(3)}
-          className={`text-black cursor-pointer ${openColumnIndex === 3 ? 'font-normal' : ''}`}
+          className={`cursor-pointer ${openColumnIndex === 0 ? 'font-normal text-black' : 'text-[#B0AFAF]'}`}
         >
-          <div className="text-gray-500 text-2xl">Finalization and Delivery</div>
+          <div className="text-[#B0AFAF] text-[20px]">Finalization and Delivery</div>
         </button>
         {columnVisibility[3] && (
-          <div className="mt-10">
-            <p className="text-2xl">
+          <div className={`${columnVisibility[3] ? 'md:col-span-4' : 'col-span-1'}`}>
+          <p className="xs:text-[14px]  md:text-[20px] xs:py-[20px] ">
               Once the design is refined and approved, we prepare the design
               files in the required formats and deliver them to the client.
             </p>
           </div>
         )}
       </div>
+      
     </div>
   );
 };
